084d274efbba497bd7b938fa921693fa2344cb1f
Xinyi Liu <94362768+XinyiLiu577086410@users.noreply.github.com>
2024-10-23 20:24:58 +0800

    add proflie_pre.sh

---
+++ profile_pre.sh
@@ -0,0 +1,7 @@
+# pre
+hipprof --hip-trace --pmc ./conv2dfp16demo 64 256 14 14 256 3 3 1 1 1 1
+hipprof --hip-trace --pmc ./conv2dfp16demo 256 192 14 14 192 3 3 1 1 1 1
+hipprof --hip-trace --pmc ./conv2dfp16demo 16 256 26 26 512 3 3 1 1 1 1
+hipprof --hip-trace --pmc ./conv2dfp16demo 32 256 14 14 256 3 3 1 1 1 1
+hipprof --hip-trace --pmc ./conv2dfp16demo 2 1280 16 16 1280 3 3 1 1 1 1
+hipprof --hip-trace --pmc ./conv2dfp16demo 2 960 64 64 32 3 3 1 1 1 1
