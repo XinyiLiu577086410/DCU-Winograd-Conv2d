4e566b857f03985e712f6aae735289f31c9b5555
Xinyi Liu <94362768+XinyiLiu577086410@users.noreply.github.com>
2024-10-26 16:58:38 +0800

    add correct final.sh

--- final.sh
+++ final.sh
@@ -1,6 +1,5 @@
 # final
 ./conv2dfp16demo 16 128 64 64 27 3 3 1 1 1 1
-./conv2dfp16demo 16 128 64 64 27 3 3 1 1 1 1
 ./conv2dfp16demo 16 256 32 32 256 3 3 1 1 1 1
 ./conv2dfp16demo 16 64 128 128 64 3 3 1 1 1 1
 ./conv2dfp16demo 2 1920 32 32 640 3 3 1 1 1 1
