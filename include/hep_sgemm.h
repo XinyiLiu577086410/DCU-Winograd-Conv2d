a725824d23f264905d31ffcd55097f420e1762e0
Xinyi Liu <94362768+XinyiLiu577086410@users.noreply.github.com>
2024-10-26 16:31:30 +0800

    Revert "" add the branch of 16x16x16""
    details:
    [xdb4_94073@xdb3 PRA24-Convolution]$ make clean && make
    rm -f conv2dfp16demo
    rm -f ./src/*.o
    /work/home/xdb4_94073/opt/dtk-24.04.1/hip/bin/hipcc -c -w src/conv2d.cpp -DHIP_ROCM -DNDEBUG -DUSE_DEFAULT_STDLIB --offload-arch=gfx928 -g -I/work/home/xdb4_94073/opt/dtk-24.04.1/hip/include -I./include -o src/conv2d.o
    /work/home/xdb4_94073/opt/dtk-24.04.1/hip/bin/hipcc -c -w src/main.cpp -DHIP_ROCM -DNDEBUG -DUSE_DEFAULT_STDLIB --offload-arch=gfx928 -g -I/work/home/xdb4_94073/opt/dtk-24.04.1/hip/include -I./include -o src/main.o
    /work/home/xdb4_94073/opt/dtk-24.04.1/hip/bin/hipcc  ./src/conv2d.o  ./src/main.o -Wl,-rpath=/opt/rh/devtoolset-7/root/usr/lib64 -Wl,-rpath=/opt/rh/devtoolset-7/root/usr/lib -o conv2dfp16demo
    [xdb4_94073@xdb3 PRA24-Convolution]$ bash ./pre.sh
    paramsize:144
    time: 512.025757 us
    ===================start verfiy===================
    ================finish,error:0=========================
    paramsize:144
    time: 1467.995117 us
    ===================start verfiy===================
    ================finish,error:0=========================
    paramsize:144
    time: 873.606018 us
    ===================start verfiy===================
    ================finish,error:0=========================
    paramsize:144
    time: 267.544006 us
    ===================start verfiy===================
    ================finish,error:0=========================
    paramsize:144
    time: 5184.043945 us
    ===================start verfiy===================
    ================finish,error:0=========================
    paramsize:144
    time: 403.405670 us
    ===================start verfiy===================
    ================finish,error:0=========================
    This reverts commit cfc659130e6ab0154f7f50443c23f226320b9578.

--- include/hep_sgemm.h
+++ include/hep_sgemm.h
@@ -17,117 +17,6 @@ union RegisterUnion
   };
 };
 
-template <typename inoutT,
-          typename calcT,
-          int  DIM_M,
-          int  DIM_N,
-          int  BLK_M,
-          int  BLK_N,
-          int  BLK_K,
-          int  DIM_M_A,
-          int  DIM_N_A,
-          int  DIM_M_B,
-          int  DIM_N_B>
-static __global__ __launch_bounds__(DIM_M * DIM_N) void
-gemm_batched_general_kernel(int32_t    M,
-                            int32_t    N,
-                            int32_t    K,
-                            inoutT      alpha,
-                            inoutT *    dA_input,
-                            int32_t    lda,
-                            inoutT *    dB_input,
-                            int32_t    ldb,
-                            inoutT      beta,
-                            inoutT *    dC_input,
-                            int32_t    ldc,
-                            int32_t    batch_count)
-{
-    int thx  = threadIdx.x; // thread's m position in C
-    int thy  = threadIdx.y; // thread's n position in C
-    int idt  = DIM_M * thy + thx; // thread's number
-    int blx  = blockIdx.x; // block's m position
-    int bly  = blockIdx.y; // block's n position
-    int blz  = blockIdx.z; // block's matrix in the batch
-    int thxA = idt % DIM_M_A; // thread's m position for loading A
-    int thyA = idt / DIM_M_A; // thread's n position for loading A
-    int thxB = idt % DIM_M_B; // thread's m position for loading B
-    int thyB = idt / DIM_M_B; // thread's n position for loading B
-
-    __shared__ calcT sA[BLK_K][BLK_M]; // shared memory for A
-    __shared__ calcT sB[BLK_N][BLK_K]; // shared memory for B
-    calcT            rC[BLK_N / DIM_N][BLK_M / DIM_M]; // registers for C
-
-    int a_i_offset = thxA + BLK_M * blx;
-    int a_j_offset = thyA;
-    int b_i_offset = thxB;
-    int b_j_offset = thyB + BLK_N * bly;
-
-    for(int n = 0; n < BLK_N / DIM_N; ++n)
-        for(int m = 0; m < BLK_M / DIM_M; ++m)
-            rC[n][m] = (calcT)0.0;
-
-    int kk = 0;
-    for(; kk < K; kk += BLK_K)
-    {
-        for(int n = 0; n < BLK_K; n += DIM_N_A)
-        {
-            for(int m = 0; m < BLK_M; m += DIM_M_A)
-            {
-                int i = m + a_i_offset;
-                int j = n + kk + a_j_offset;
-                if(i < M && j < K)
-                {
-                    sA[n + thyA][m + thxA] = (calcT)dA_input[i * size_t(lda) + j];
-                }
-                else
-                {
-                    sA[n + thyA][m + thxA] = (calcT)0.0;
-                }
-            }
-        }
-
-        for(int n = 0; n < BLK_N; n += DIM_N_B)
-        {
-            for(int m = 0; m < BLK_K; m += DIM_M_B)
-            {
-                int i = m + kk + b_i_offset;
-                int j = n + b_j_offset;
-                if(i < K && j < N)
-                {
-                    sB[n + thyB][m + thxB] = (calcT)dB_input[i + j * size_t(ldb)];
-                }
-                else
-                {
-                    sB[n + thyB][m + thxB] = (calcT)0;
-                }
-            }
-        }
-
-        __syncthreads();
-
-        for(int k = 0; k < BLK_K; ++k)
-            for(int n = 0; n < BLK_N / DIM_N; ++n)
-                for(int m = 0; m < BLK_M / DIM_M; ++m)
-                    rC[n][m] += sA[k][m * DIM_M + thx] * sB[n * DIM_N + thy][k];
-
-        __syncthreads();
-    }
-
-    for(int n = 0; n < BLK_N / DIM_N; ++n)
-    {
-        for(int m = 0; m < BLK_M / DIM_M; ++m)
-        {
-            int coord_dCm = blx * BLK_M + m * DIM_M + thx;
-            int coord_dCn = bly * BLK_N + n * DIM_N + thy;
-            if(coord_dCn < N && coord_dCm < M)
-            {
-                dC_input[coord_dCn * size_t(ldc) + coord_dCm] = (inoutT)alpha * rC[n][m];
-            }
-        }
-    }
-}
-
-
 template <int  BLK_M,
           int  BLK_N,
           int  BLK_K>
@@ -355,79 +244,6 @@ gemm_batched_kernel_tensorcore_32x32x16_fp16fp32
     dC_input[offset_C3 + 12 * size_t(ldc)] = (_Float16)C_reg_acc[3].w;
 }
 
-
-template <int  BLK_M,
-          int  BLK_N,
-          int  BLK_K>
-static __global__ __launch_bounds__(HEP_WARP_SIZE) void
-gemm_batched_kernel_tensorcore_16x16x16_fp16fp32
-                   (int32_t    M,
-                    int32_t    N,
-                    int32_t    K,
-                    _Float16*  dA_input,
-                    int32_t    lda,
-                    _Float16*  dB_input,
-                    int32_t    ldb,
-                    _Float16*  dC_input,
-                    int32_t    ldc)
-{
-    static_assert(BLK_M == 16 && BLK_N == 16 && BLK_K == 16, "incorrect block shape");
-    int thx  = threadIdx.x;
-    int blx  = blockIdx.x;  // block's m position
-    int bly  = blockIdx.y;  // block's n position
-    __shared__ struct {
-        fp16 AB[BLK_M][2][BLK_K]; // shared memory for A, B
-    } lds;
-
-    fp32x4 C_reg_acc = {0, 0, 0, 0};
-
-    int kk = 0;
-    for(; kk < K; kk += BLK_K)
-    {
-        size_t read_dim_k  = thx % BLK_K;
-        size_t read_dim_mn = thx / BLK_K;
-        size_t offset_A = size_t(kk + read_dim_k) + size_t(blx * BLK_M + read_dim_mn) * size_t(lda);
-        size_t offset_B = size_t(kk + read_dim_k) + size_t(bly * BLK_N + read_dim_mn) * size_t(ldb);
-
-        lds.AB[read_dim_k][0][read_dim_mn +  0] = dA_input[offset_A +  0 * size_t(lda)];
-        lds.AB[read_dim_k][0][read_dim_mn +  4] = dA_input[offset_A +  4 * size_t(lda)];
-        lds.AB[read_dim_k][0][read_dim_mn +  8] = dA_input[offset_A +  8 * size_t(lda)];
-        lds.AB[read_dim_k][0][read_dim_mn + 12] = dA_input[offset_A + 12 * size_t(lda)];
-
-        lds.AB[read_dim_k][1][read_dim_mn +  0] = dB_input[offset_B +  0 * size_t(ldb)];
-        lds.AB[read_dim_k][1][read_dim_mn +  4] = dB_input[offset_B +  4 * size_t(ldb)];
-        lds.AB[read_dim_k][1][read_dim_mn +  8] = dB_input[offset_B +  8 * size_t(ldb)];
-        lds.AB[read_dim_k][1][read_dim_mn + 12] = dB_input[offset_B + 12 * size_t(ldb)];
-        asm volatile("s_waitcnt lgkmcnt(0)\n\t");
-
-        RegisterUnion fragAB;
-        fp32x4 C_reg_res = {0, 0, 0, 0};
-        int lds_read_offset = (thx * 8) * sizeof(_Float16);
-
-        read_dim_mn = thx % BLK_M;
-        read_dim_k  = thx / BLK_M * 4;
-        fragAB.vector_front = {lds.AB[read_dim_k + 0][0][read_dim_mn], lds.AB[read_dim_k + 1][0][read_dim_mn], lds.AB[read_dim_k + 2][0][read_dim_mn], lds.AB[read_dim_k + 3][0][read_dim_mn]};
-        fragAB.vector_rear  = {lds.AB[read_dim_k + 0][1][read_dim_mn], lds.AB[read_dim_k + 1][1][read_dim_mn], lds.AB[read_dim_k + 2][1][read_dim_mn], lds.AB[read_dim_k + 3][1][read_dim_mn]};
-
-        __syncthreads();
-        // asm volatile("ds_read_m32x16_b16 %0, %1 offset:0\n\t": "+v"(fragAB.vector8), "+v"(lds_read_offset));
-        asm volatile("s_waitcnt lgkmcnt(0)\n\t");
-        asm volatile("v_mmac_f32_16x16x16_f16 %0, %1, %2, %0\n\t":"+v"(C_reg_acc), "+v"(fragAB.vector_front), "+v"(fragAB.vector_rear));
-
-        // C_reg_acc += C_reg_res;
-    }
-    __syncthreads();
-
-    size_t output_row = thx % 16;
-    size_t output_col = thx / 16;
-    size_t offset_C = size_t(bly * BLK_N + output_col) * size_t(ldc) + size_t(blx * BLK_M + output_row);
-
-    dC_input[offset_C +  0 * size_t(ldc)] = (_Float16)C_reg_acc.x;
-    dC_input[offset_C +  4 * size_t(ldc)] = (_Float16)C_reg_acc.y;
-    dC_input[offset_C +  8 * size_t(ldc)] = (_Float16)C_reg_acc.z;
-    dC_input[offset_C + 12 * size_t(ldc)] = (_Float16)C_reg_acc.w;
-}
-
 template <typename inoutT, typename calcT>
 static void hep_sgemm(int32_t       m,
                       int32_t       n,
@@ -455,20 +271,8 @@ static void hep_sgemm(int32_t       m,
         dim3      dimGrid(m / blk_m, n / blk_n, batch_count);
         gemm_batched_kernel_tensorcore_32x32x16_fp16fp32<blk_m, blk_n, blk_k><<<dimGrid, dimBlock, 0, stream>>>(m, n, k, dA, lda, dB, ldb, dC, ldc);
     }
-    else if((m % 16 == 0) && (n % 16 == 0) && (k % 16 == 0))
-    {
-        std::cout << "this kernel not implemented!!!" << std::endl;
-        std::exit(-1);
-        // m is mult of 16, n is mult of 16, k is mult of 16
-        // const int blk_m = 16;
-        // const int blk_n = 16;
-        // const int blk_k = 16;
-        // dim3      dimBlock(HEP_WARP_SIZE);
-        // dim3      dimGrid(m / blk_m, n / blk_n, 1);
-        // gemm_batched_kernel_tensorcore_16x16x16_fp16fp32<blk_m, blk_n, blk_k><<<dimGrid, dimBlock, 0, stream>>>(m, n, k, dA, lda, dB, ldb, dC, ldc);
-    }
     else
-    {   
+    {
         const int blk_m = 32;
         const int blk_n = 32;
         const int blk_k = 16;
